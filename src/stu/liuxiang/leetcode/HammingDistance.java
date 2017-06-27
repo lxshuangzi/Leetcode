c606e3d1e03eebef7570d8c1e0f708610ff1ded8
lxshuangzi <55578625@qq.com>
2017-06-27 11:46:28 +0800

    1.算法BUG修正

--- src/stu/liuxiang/leetcode/HammingDistance.java
+++ src/stu/liuxiang/leetcode/HammingDistance.java
@@ -2,25 +2,35 @@ package stu.liuxiang.leetcode;
 
 public class HammingDistance {
 	public static void main(String[] args) {
-		int a = 9;
-		int b = 4;
+		int a = 1;
+		int b = 127;
 		HammingDistance hm = new HammingDistance();
 		// hm.test();
 		System.out.println("result======" + hm.getHammingDistance1(a, b));
+		System.out.println("result======" + hm.getHammingDistance2(a, b));
 	}
 
 	private int getHammingDistance1(int a, int b) {
 		int z = a ^ b;
 		int count = 0;
-		while (true) {
-			if (z / 2 > 1) {
-				z = z / 2;
-				count++;
-			} else {
-				count++;
-				break;
-			}
+		System.out.println("a^b ==" + z);
+		for(int i = 0; i < 32 ; i++){
+			int z2 = z>>i;
+			count += z2&1;
 		}
-		return z;
+		return count;
+	}
+	private int getHammingDistance2(int a,int b){
+		int max = a >= b ? a : b;
+		int count = 0;
+		while(max!=0){
+			if(a%2 != b%2){
+				count ++;
+			}
+			a = a>>1;
+			b = b>>1;
+			max = max >>1;
+		}
+		return count;
 	}
 }
